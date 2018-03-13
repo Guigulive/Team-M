35e2a4516db09321de382a6fe6e8b4fb566b4762
Haorui Wu <srxiaoj@gmail.com>
2018-03-13 00:10:07 -0700

    First assigment

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,40 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+// author: @haoruiwu
+contract Payroll {
+    uint salary = 1 ether;
+    address employee;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+    
+    function setSalary(uint s) {
+        salary = s * 1 ether;
+    }
+    
+    function setEmployee(address e) {
+        employee = e;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunaway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunaway() > 0;
+    }
+    
+    function getPaid() {
+        require (msg.sender == employee);
+        uint nextPayDay = lastPayday + payDuration;
+        if (nextPayDay > now) {
+            revert();
+        }
+        
+        lastPayday = nextPayDay;
+        employee.transfer(salary);
+    }
+}
