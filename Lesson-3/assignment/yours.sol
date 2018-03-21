727f4ca38b684f96d52abe73e334dd929a0b53d5
nino <qweng@stu.xidian.edu.cn>
2018-03-21 09:56:12 +0800

    第二题答案

--- Lesson-3/assignment/yours.sol
+++ Lesson-3/assignment/yours.sol
@@ -1,1 +1,7 @@
 /*作业请提交在这个目录下*/
+第二题：增加 changePaymentAddress 函数，更改员工的薪水支付地址，思考一下能否使用modifier整合某个功能
+
+function changePaymentAddress(address employeeId,address newId) onlyOwner employeeExist(employeeId){
+        var employee = employees[employeeId];        
+        employee.id = newId;
+}
