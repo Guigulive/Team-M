e1e0a451fffab55fcd119712b56c66fb716f78ad
nino <qweng@stu.xidian.edu.cn>
2018-03-25 21:14:33 +0800

    122-翁启-第四次作业

--- Lesson-4/assignment/yours.sol
+++ Lesson-4/assignment/yours.sol
@@ -1,1 +1,32 @@
 /*作业请提交在这个目录下*/
+写出对如下两个函数的单元测试：
+function addEmployee(address employeeId, uint salary) onlyOwner
+function removeEmployee(address employeeId) onlyOwner employeeExist(employeeId)
+
+var Payroll = artifacts.require("./Payroll.sol");
+
+contract('Payroll', function(accounts) {
+
+  it("should add and remove employee correctly", function() {
+    var payroll;
+    var account_boss = accounts[0];
+    var account_employee = accounts[1];
+
+    return Payroll.deployed().then(function(instance) {
+      payroll = instance;
+      assert(true);
+      return payroll.addEmployee(account_employee, 2);
+    }).then(function() {
+      return payroll.checkEmployee(account_employee);
+    }).then(function(result) {
+      assert.equal(result[0].toString(), web3.toWei(2, 'ether'), "salary wasn't correct");
+    }).then(function() {
+      return payroll.removeEmployee(account_employee);
+    }).then(function() {
+      return payroll.checkEmployee(account_employee);
+    }).then(function(result) {
+      assert.equal(result[0].toString(), '0', "salary wasn't empty");
+      assert.equal(result[1].toString(), '0', "lastPayday wasn't empty");
+    })
+   });
+});
